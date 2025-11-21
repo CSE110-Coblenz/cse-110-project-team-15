142bf39d8735bc497dc1d0993f88b18413cbb40e
cybedra <147957633+cybedra@users.noreply.github.com>
2025-11-20 19:13:44 -0800

    add skeleton for sync

--- backend/routers/sync.py
+++ backend/routers/sync.py
@@ -1,10 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import JSONResponse
+
+import asyncpg  # type: ignore[import]
+
+from core.dependencies import get_db_connection
+from core.security import hash_password, verify_password
+from models.users import AuthResponse, LoginRequest, RegisterRequest
 from models.save import SyncResponse
 from repo.saves import get_save
 
 game_sync_router = APIRouter(tags=["game"])
-USERNAME = "demo"
 
 @game_sync_router.get("/sync", response_model=SyncResponse)
-async def handle_data_sync():
-    return SyncResponse(**get_save(USERNAME).model_dump())
+async def handle_data_sync(
+    connection: asyncpg.Connection = Depends(get_db_connection),
+    user=Depends(get_session_id)
+):
+    pass
+    # return SyncResponse(**get_save(USERNAME).model_dump())
